b7f95340a0ead7d302ebef6786424e5bd8060065
amauritrading <adm.neuropsicosaude@gmail.com>
2026-04-26 14:52:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -473,3 +473,18 @@ def executar(
             "status": "erro",
             "erro": str(e)
         }
+import requests
+import os
+
+def enviar_telegram(mensagem):
+    token = os.getenv("TELEGRAM_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    payload = {
+        "chat_id": chat_id,
+        "text": mensagem
+    }
+
+    requests.post(url, json=payload)
