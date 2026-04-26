c9e189351cd426270cc3a5a55721b0cf0404430d
amauritrading <adm.neuropsicosaude@gmail.com>
2026-04-26 07:48:25 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+# Rota teste
+@app.get("/")
+def home():
+    return {"status": "online", "sistema": "trading-ai"}
+
+# Buscar preço da Binance
+@app.get("/preco/{symbol}")
+def get_preco(symbol: str):
+    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol.upper()}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        return {"erro": "falha ao buscar preço"}
+
+    data = response.json()
+    return {
+        "ativo": symbol.upper(),
+        "preco": data["price"]
+    }
