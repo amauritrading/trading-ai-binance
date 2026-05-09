e7420fba2535f1a8623ae6d4cc18f99a72b0de54
amauritrading <adm.neuropsicosaude@gmail.com>
2026-05-09 13:10:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -410,7 +410,7 @@ def gerar_analise(symbol):
     distancia_resistencia = (resistencia_curta - preco) / preco
     distancia_suporte = (preco - suporte_curto) / preco
 
-    espaco_ate_alvo = distancia_resistencia >= 0.013
+    espaco_ate_alvo = distancia_resistencia >= 0.008
 
     return {
         "ativo": symbol,
